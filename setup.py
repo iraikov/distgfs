beda5602e94bd80ea06a25bb9d0d70f13e2d941d
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-09-07 18:58:52 -0700

    bug fix in restoring constraints from file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="distgfs",
-    version="0.1.0",
+    version="0.1.1",
     author="Ivan Raikov",
     author_email="ivan.g.raikov@gmail.com",
     description="Distributed global function search via dlib.",
